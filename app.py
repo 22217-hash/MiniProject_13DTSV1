f36d9ba41db4571d11ec2c095634450262d5185a
22217 <22217@wc.school.nz>
2026-03-19 11:05:33 +1300

    added login function, added app routes to app.py

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_tenplate, request, redirect, session
+from flask import Flask, render_template, redirect
 import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
 
 def connect_db():
-    return sqlite3.connect("database.db"
+    return sqlite3.connect("database.db")
 
 
 
 
 @app.route('/')
 def home():  # put application's code here
-    return redirect("/")
+    return render_template("home.html")
+
+@app.route('/signup', methods=['POST','GET'])
+def render_signup_page():  # put application's code here
+    return render_template("signup.html")
+
+@app.route('/login', methods=['POST','GET'])
+def render_login_page():  # put application's code here
+    return render_template("login.html")
 
 
 # sign up - input info into the database
