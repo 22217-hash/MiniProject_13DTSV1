4f1dae9d1d9d96fae33441c69718762454683fb7
22217 <22217@wc.school.nz>
2026-03-20 12:28:49 +1300

    added request for information from signup, corected the data for first name capital last name capital and no spaces and keep email lower case.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 import sqlite3
 from sqlite3 import Error
 
@@ -15,9 +15,21 @@ def home():  # put application's code here
     return render_template("home.html")
 
 @app.route('/signup', methods=['POST','GET'])
-def render_signup_page():  # put application's code here
-    return render_template("signup.html")
+def render_signup_page():   # put application's code here
+    if request.method == 'POST':
+        fname = request.form.get('user_fname').title().strip()
+        lmame = request.form.get('user_lname').title().strip()
+        email = request.form.get('user_email').lower().strip()
+        password = request.form.get('user_password')
+        password2 = request.form.get('user_password2')
 
+        if password != password2:
+            return  redirect("\signup?error=passwords+do+not+match")
+
+        if int(password) < 8:
+            return redirect("\signup?error=password+must+be+over+8+characters")
+
+    return render_template("signup.html")
 @app.route('/login', methods=['POST','GET'])
 def render_login_page():  # put application's code here
     return render_template("login.html")
